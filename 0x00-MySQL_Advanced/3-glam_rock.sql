f84626ed7f45e7b38ae226463a4adc3b5449947e
Stephen Gitau <stephenpepe.sm@gmail.com>
2024-01-24 12:41:50 -0800

    selcet band name where main style is glam_rock orderby lifespan

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- select band_name where style is glam rock order in DESC by longevity
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC
